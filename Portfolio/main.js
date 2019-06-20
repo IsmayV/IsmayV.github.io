75db3639d42fd69ee21f2895fd21bf00bea6d132
IsmayV <IsmayV@users.noreply.github.com>
2019-06-20 12:58:15 +0200

    Revert "blub"
    This reverts commit 5ef4e7375528ce4dbb8ab12c00c6d9afc68f07a9.

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -106,6 +106,4 @@ $(document).ready(function() {
     $('#button-digital').click(toggleSectionDigital);
     $('#button-photography').click(toggleSectionPhotography);
     $('.portfolio-image').click(showDescription);
-    const observer = lozad();
-    observer.observe();
 });
